bfd3b61b33956fa502dfb652625f4d982ebfbdb6
nizamkolathoden <nizamniz100@gmail.com>
2021-01-21 23:45:57 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const DB = require('./config/DB')
-
+const f = require('../masc-sdb-1.0/build')
 //env
 const dot = require('dotenv');
 dot.config({ path: './config/key.env' })
@@ -27,6 +27,14 @@ app.use('/subject', require('./routes/subjects'));
 app.use('/profile', require('./routes/profileStudent'));
 //for remarks
 app.use('/remark', require('./routes/remark'))
+
+if(process.env.NODE_ENV=='Production'){
+    app.use(express.static('../masc-sdb-1.0/build'))
+    const path = require('path')
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 //listening the server
 app.listen(Port, () => console.log(`\u{1F525}\u{1F680} server running on ${process.env.NODE_ENV} in Port ${Port} \u{1F525}\u{1F680}\u{1F525} `))
